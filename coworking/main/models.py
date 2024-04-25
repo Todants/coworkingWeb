2369094eb970fca8f9d37e086897983908863fc7
Todants <59180041+Todants@users.noreply.github.com>
2024-04-25 22:47:25 +0300

    +database

--- coworking/main/models.py
+++ coworking/main/models.py
@@ -2,12 +2,47 @@ from django.db import models
 
 
 class Users(models.Model):
-    email = models.CharField(max_length=50, blank=False)
-    password = models.CharField(max_length=50, blank=False)
-    first_name = models.CharField(max_length=50, blank=False)
-    last_name = models.CharField(max_length=50, blank=False)
-    phone_number = models.CharField(max_length=50, blank=False)
+    email = models.CharField(max_length=50, blank=False, null=False)
+    password = models.CharField(max_length=50, blank=False, null=False)
+    first_name = models.CharField(max_length=50, blank=False, null=False)
+    last_name = models.CharField(max_length=50, blank=False, null=False)
+    phone_number = models.CharField(max_length=50)
     date_of_birth = models.DateField(null=False)
 
     def __str__(self):
         return self.first_name
+
+
+class Businesses(models.Model):
+    company_name = models.CharField(max_length=50, blank=False, null=False)
+    email = models.CharField(max_length=50, blank=False, null=False)
+    password = models.CharField(max_length=50, blank=False, null=False)
+    phone_number = models.CharField(max_length=50)
+
+
+class CoworkingSpaces(models.Model):
+    id_company = models.BigIntegerField(null=False)
+    description = models.CharField(max_length=50, blank=False, null=False)
+    date_start = models.TimeField(null=False)
+    date_end = models.TimeField(null=False)
+
+
+class Images(models.Model):
+    id_coworking = models.BigIntegerField(null=False)
+    file = models.FileField(upload_to='upldfile/')
+
+
+class Bookings(models.Model):
+    id_coworking = models.BigIntegerField(null=False)
+    id_user = models.BigIntegerField(null=False)
+    price = models.BigIntegerField(null=False)
+    type = models.CharField(max_length=50, blank=False, null=False)
+    date_start = models.DateTimeField(null=False)
+    date_end = models.DateTimeField(null=False)
+
+
+class Services(models.Model):
+    id_coworking = models.BigIntegerField(null=False)
+    price = models.BigIntegerField(null=False)
+    type = models.CharField(max_length=50, blank=False, null=False)
+    num_of_seats = models.BigIntegerField(null=True)
