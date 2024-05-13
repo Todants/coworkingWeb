2de05a8cbe0c69c31cb768ea41c9b183f9bd2d9d
Todants <59180041+Todants@users.noreply.github.com>
2024-05-13 23:38:51 +0300

    фикс перехода из страницы коворкинга

--- coworking/main/urls.py
+++ coworking/main/urls.py
@@ -4,10 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('login', views.login_view, name='login_view'),
-    path('about', views.about),
-    path('contacts', views.contacts),
-    path('profile', views.profile),
-    path('book', views.temp),
+    path('about', views.about, name='about'),
+    path('contacts', views.contacts, name='contacts'),
+    path('profile', views.profile, name='profile'),
+    path('book', views.temp, name='book'),
     path('coworking/<int:cowork_id>', views.coworking),
     path('registration', views.registration, name='registration'),
 
